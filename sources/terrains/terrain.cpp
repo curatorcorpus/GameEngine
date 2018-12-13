fb17dd9883ed49d7fcbeac59a665d70678401a1d
curatorcorpus <itachi383@hotmail.com>
2018-12-13 23:00:43 +1300

    grass texture added

--- sources/terrains/terrain.cpp
+++ sources/terrains/terrain.cpp
@@ -12,13 +12,17 @@ void Terrain::generate_terrains()
     std::vector<glm::vec2>      uvs;
     std::vector<unsigned short> indices;
 
-    float y = 0.0;
+    int offset = 0;
+    float y = 0.0f;
 
-    for(float x_idx = 0; x_idx < VERTICES_NO; x_idx++)
+    for(int z_idx = 0; z_idx < VERTICES_NO; z_idx++)
     {
-        for(float z_idx = 0; z_idx < VERTICES_NO; z_idx++)
+        for(int x_idx = 0; x_idx < VERTICES_NO; x_idx++)
         {
-            
+            // generate vertices
+            float x = (float) _idx / ((float) VERTICES_NO - 1) * SIZE;
+
+            //glm::vec3 vert();
         }
     }   
 
