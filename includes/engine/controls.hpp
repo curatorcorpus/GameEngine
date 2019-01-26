d58d6a09aafa1c46fa0783d876433e6238f342b5
curatorcorpus <itachi383@hotmail.com>
2019-01-26 22:01:33 +1300

    img saving to png encapsulated in ImgWriter class.

--- includes/engine/controls.hpp
+++ includes/engine/controls.hpp
@@ -14,6 +14,7 @@
 #include <png.h>
 
 #include <camera.hpp>
+#include <img_writer.hpp>
 
 /*
 	External Libraries
@@ -51,53 +52,14 @@ public:
 		if(key == GLFW_KEY_F12 && action == GLFW_PRESS)
 		{
 			std::cerr << "[DEBUG::CONTROLS] Screenshot saved!" << std::endl;
-			int w = 1280;
+			int w = 1920;
 			int h = 1080;
 			std::string file_name = "./test.png";
+			//glfwGetWindowSize(window, &w, &h); [THIS BREAKS]
+		
 			uint8_t *pixels = new uint8_t[w * h * 3];
-			glReadPixels(0, 0, 1280, 1080, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid *)pixels);
-
-		    png_structp png = png_create_write_struct(PNG_LIBPNG_VER_STRING, nullptr, nullptr, nullptr);
-			if (!png)
-				return;
-
-			png_infop info = png_create_info_struct(png);
-			if (!info) {
-				png_destroy_write_struct(&png, &info);
-				return ;
-			}
-
-			FILE *fp = fopen(file_name.c_str(), "wb");
-			if (!fp) {
-				png_destroy_write_struct(&png, &info);
-				return;
-			}
-
-			png_init_io(png, fp);
-			png_set_IHDR(png, info, w, h, 8 /* depth */, PNG_COLOR_TYPE_RGB, PNG_INTERLACE_NONE,
-				PNG_COMPRESSION_TYPE_BASE, PNG_FILTER_TYPE_BASE);
-			png_colorp palette = (png_colorp)png_malloc(png, PNG_MAX_PALETTE_LENGTH * sizeof(png_color));
-			if (!palette) {
-				fclose(fp);
-				png_destroy_write_struct(&png, &info);
-				return;
-			}
-			png_set_PLTE(png, info, palette, PNG_MAX_PALETTE_LENGTH);
-			png_write_info(png, info);
-			png_set_packing(png);
-
-			png_bytepp rows = (png_bytepp)png_malloc(png, h * sizeof(png_bytep));
-			for (int i = 0; i < h; ++i)
-				rows[i] = (png_bytep)(pixels + (h - i - 1 ) * w * 3);
-
-			png_write_image(png, rows);
-			png_write_end(png, info);
-			png_free(png, palette);
-			png_destroy_write_struct(&png, &info);
-
-			fclose(fp);
-			delete[] rows;
-			return;
+			glReadPixels(0, 0, w, h, GL_RGB, GL_UNSIGNED_BYTE, (GLvoid *)pixels);
+			ImgWriter::save_png(file_name, w, h, pixels);
 		}
 	}
 
