05c7c9460ce9f0e95ddc5c1c8e4259c5f9124ba4
curatorcorpus <itachi383@hotmail.com>
2019-03-09 01:44:26 +1300

    perlin noise class added to cmake, not impl yet. utilities folder renamed. perlin noise source and header renamed

---
+++ includes/utilities/perlin_noise.hpp
@@ -0,0 +1,62 @@
+#ifndef PerlinNoise_HPP_
+#define PerlinNoise_HPP_
+
+/*
+    C Includes.
+*/
+
+
+/*
+    C++ Includes.
+*/
+#include <iostream>
+
+/*
+    Project Includes.
+*/
+
+
+class PerlinNoise {
+
+private:
+
+    // CONSTANTS
+
+
+    // DATAFIELDS
+    
+
+    // OVERRIDE-ABLE METHODS
+
+
+	// METHODS
+
+
+    // GETTERS
+
+
+    // SETTERS
+
+
+public:
+
+    // CONSTRUCTORS
+    PerlinNoise();
+
+    // DESTRUCTORS
+    ~PerlinNoise();
+
+   	// OVERRIDE-ABLE METHODS
+
+
+	// METHODS
+
+
+    // GETTERS
+
+
+    // SETTERS
+
+};
+
+#endif
